931d5404497a2a466d614a723806fe7c7c8ce2a2
Dierish05 <dierish.a.rojas@gmail.com>
2023-11-20 18:59:38 -0600

    frmVendedor

--- ProyectoCursoSoftware/Formularios/FormVendedor.Designer.cs
+++ ProyectoCursoSoftware/Formularios/FormVendedor.Designer.cs
@@ -28,19 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            btnBuscar = new Button();
+            dataGridView1 = new DataGridView();
+            btnAgregar = new Button();
+            btnEditar = new Button();
+            btnEliminar = new Button();
+            btnSalir = new Button();
+            btnActualizar = new Button();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            label1.BackColor = Color.Teal;
+            label1.Font = new Font("Arial", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(-4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(1155, 50);
+            label1.TabIndex = 0;
+            label1.Text = "BUSQUEDA Y MANTENIMIENTO DE VENDEDOR";
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(btnBuscar);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Location = new Point(12, 72);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(350, 112);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Buscar";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(19, 27);
+            label2.Name = "label2";
+            label2.Size = new Size(67, 20);
+            label2.TabIndex = 0;
+            label2.Text = "Nombre:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(89, 27);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(193, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // btnBuscar
+            // 
+            btnBuscar.Location = new Point(6, 60);
+            btnBuscar.Name = "btnBuscar";
+            btnBuscar.Size = new Size(94, 29);
+            btnBuscar.TabIndex = 2;
+            btnBuscar.Text = "Buscar";
+            btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 215);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.RowTemplate.Height = 29;
+            dataGridView1.Size = new Size(991, 337);
+            dataGridView1.TabIndex = 2;
+            // 
+            // btnAgregar
+            // 
+            btnAgregar.Location = new Point(1021, 355);
+            btnAgregar.Name = "btnAgregar";
+            btnAgregar.Size = new Size(94, 29);
+            btnAgregar.TabIndex = 3;
+            btnAgregar.Text = "Nuevo";
+            btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // btnEditar
+            // 
+            btnEditar.Location = new Point(1021, 408);
+            btnEditar.Name = "btnEditar";
+            btnEditar.Size = new Size(94, 29);
+            btnEditar.TabIndex = 4;
+            btnEditar.Text = "Editar";
+            btnEditar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.Location = new Point(1021, 466);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(94, 29);
+            btnEliminar.TabIndex = 5;
+            btnEliminar.Text = "Eliminar";
+            btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            btnSalir.Location = new Point(1021, 523);
+            btnSalir.Name = "btnSalir";
+            btnSalir.Size = new Size(94, 29);
+            btnSalir.TabIndex = 6;
+            btnSalir.Text = "Salir";
+            btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // btnActualizar
+            // 
+            btnActualizar.Location = new Point(909, 169);
+            btnActualizar.Name = "btnActualizar";
+            btnActualizar.Size = new Size(94, 29);
+            btnActualizar.TabIndex = 7;
+            btnActualizar.Text = "Actualizar";
+            btnActualizar.UseVisualStyleBackColor = true;
             // 
             // FormVendedor
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(37, 42, 64);
-            ClientSize = new Size(800, 450);
+            BackColor = SystemColors.ControlLight;
+            ClientSize = new Size(1137, 595);
+            Controls.Add(btnActualizar);
+            Controls.Add(btnSalir);
+            Controls.Add(btnEliminar);
+            Controls.Add(btnEditar);
+            Controls.Add(btnAgregar);
+            Controls.Add(dataGridView1);
+            Controls.Add(groupBox1);
+            Controls.Add(label1);
+            ForeColor = SystemColors.ControlText;
             Name = "FormVendedor";
             Text = "FormVendedor";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Label label1;
+        private GroupBox groupBox1;
+        private Button btnBuscar;
+        private TextBox textBox1;
+        private Label label2;
+        private DataGridView dataGridView1;
+        private Button btnAgregar;
+        private Button btnEditar;
+        private Button btnEliminar;
+        private Button btnSalir;
+        private Button btnActualizar;
     }
 }
