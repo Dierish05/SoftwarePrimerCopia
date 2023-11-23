06e904683349cb99020edd5611131cf7e048899e
josue320 <josue.flores68u@std.uni.edu.ni>
2023-11-22 22:22:07 -0600

    xd

--- ProyectoCursoSoftware/Model/VendedorModel.cs
+++ ProyectoCursoSoftware/Model/VendedorModel.cs
@@ -37,30 +37,36 @@ namespace ProyectoCursoSoftware.Model
 
         }
 
-        public void CrearVendedor(String Nombres, string Apellidos, string Dir, string tel, char sex, int mun, string email, string NCedula)
+        public void CrearVendedor(String PNV, String SNV, String PAV, string SAV, string Dir, string tel, char sex, int mun, string email, string NCedula, string usuario)
         {
 
             try
             {
                 SqlCommand cmd = new SqlCommand();
 
-                SqlParameter[] param = new SqlParameter[8];
-                param[0] = new SqlParameter("@Nombres", SqlDbType.NVarChar);
-                param[0].Value = Nombres;
-                param[1] = new SqlParameter("@Apellidos", SqlDbType.NVarChar);
-                param[1].Value = Apellidos;
-                param[2] = new SqlParameter("@Dir", SqlDbType.NVarChar);
-                param[2].Value = Dir;
-                param[3] = new SqlParameter("@Tel", SqlDbType.Char);
-                param[3].Value = tel;
-                param[4] = new SqlParameter("@Sexo", SqlDbType.Char);
-                param[4].Value = sex;
-                param[5] = new SqlParameter("@Id_Mun", SqlDbType.Int);
-                param[5].Value = mun;
-                param[6] = new SqlParameter("@Email", SqlDbType.NVarChar);
-                param[6].Value = email;
-                param[7] = new SqlParameter("@NCedulaEmp", SqlDbType.Char);
-                param[7].Value = NCedula;
+                SqlParameter[] param = new SqlParameter[11];
+                param[0] = new SqlParameter("@PNV", SqlDbType.NVarChar);
+                param[0].Value = PNV;
+                param[1] = new SqlParameter("@SNV", SqlDbType.NVarChar);
+                param[1].Value = SNV;
+                param[2] = new SqlParameter("@PAV", SqlDbType.NVarChar);
+                param[2].Value = PAV;
+                param[3] = new SqlParameter("@SAV", SqlDbType.NVarChar);
+                param[3].Value = SAV;
+                param[4] = new SqlParameter("@Dir", SqlDbType.NVarChar);
+                param[4].Value = Dir;
+                param[5] = new SqlParameter("@Tel", SqlDbType.Char);
+                param[5].Value = tel;
+                param[6] = new SqlParameter("@Sexo", SqlDbType.Char);
+                param[6].Value = sex;
+                param[7] = new SqlParameter("@Id_Mun", SqlDbType.Int);
+                param[7].Value = mun;
+                param[8] = new SqlParameter("@Email", SqlDbType.NVarChar);
+                param[8].Value = email;
+                param[9] = new SqlParameter("@NCedulaEmp", SqlDbType.Char);
+                param[9].Value = NCedula;
+                param[10] = new SqlParameter("@usuario", SqlDbType.Char);
+                param[10].Value = usuario;
 
 
                 cmd.CommandType = CommandType.StoredProcedure;
@@ -156,9 +162,9 @@ namespace ProyectoCursoSoftware.Model
                 SqlCommand cmd = new SqlCommand();
 
                 SqlParameter[] param = new SqlParameter[2];
-                param[0] = new SqlParameter("@@Usuario", SqlDbType.NVarChar);
+                param[0] = new SqlParameter("@Usuario", SqlDbType.NVarChar);
                 param[0].Value = usuario;
-                param[1] = new SqlParameter("@@Contraseña", SqlDbType.NVarChar);
+                param[1] = new SqlParameter("@Contraseña", SqlDbType.NVarChar);
                 param[1].Value = contrasena;
 
 
